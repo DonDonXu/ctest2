a97328021e8f14bf0a26ecf8acdc6f17e61a58f4
yizhuspace <capricorn_don@hotmail.com>
2022-10-20 14:53:03 +0800

    cget

--- test1.c
+++ test1.c
@@ -1,9 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+//一个字符，一个字符读写文件；
 
 int main(int argc, char *argv[]){
 
     FILE *fp;
     fp = fopen("/home/donshu/ctest2/aaa", "r");
+    if(fp != NULL){
+        char c;
+        while(1){
+            c = getc(fp);
+            if(c == EOF) break;
+            printf("%c", c);
+        }
+    }else{
+        printf("打开文件失败；\n");
+        return EXIT_FAILURE;
+    }
     fclose(fp);
     return 0;
 }
