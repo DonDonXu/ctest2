9f402d21b9960659105ed903e4a4ec12461b897a
yizhuspace <capricorn_don@hotmail.com>
2022-10-27 17:41:04 +0800

    rewind

--- file_seek_rewind2.c
+++ file_seek_rewind2.c
@@ -4,15 +4,16 @@
 
 #define FILE_LINE_LEN 1024
 
-void read_offset(char *file){
+long read_offset(char *file){
     long offset_buff;
     FILE *fp = fopen(file, "r");
     if (!fp) {  
         printf("cant open file, file: %s\n", file);  
-        return -2;
+        offset_buff = 0;
     } 
     fscanf(fp, "%ld", &offset_buff);
     fclose(fp);
+    return offset_buff;
 }
 
 void write_offset(char *file, long offset){
@@ -29,17 +30,21 @@ void write_offset(char *file, long offset){
 int main(){
     char text[FILE_LINE_LEN];
     FILE *fp = fopen("/home/donshu/ctest2/bbb", "r");
-    int position = 0;
+    long position;
+    read_offset("/tmp/aaa", &position);
+    fseek(fp, position, SEEK_SET);
+
     while(1){
         memset(text, 0x0, FILE_LINE_LEN);
         fgets(text, FILE_LINE_LEN, fp);
-        position = ftell(fp);
+        
         write_offset("/tmp/aaa", position);
         if(feof(fp) == 1){
             fseek(fp, position, SEEK_SET);
         };
         sleep(1);
         printf("%s", text);
+        position = ftell(fp);
     }
     puts("程序结束了");
     fclose(fp);
